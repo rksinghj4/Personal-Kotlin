89d1d7a12bf17609c5a926f8d09e47ecef8c54c1
Raj Kumar Singh <rksinghj4@gmail.com>
2024-10-12 09:04:28 +0530

    Added Cold and Hot Flow

---
+++ FlowsChannels/app/src/main/java/com/example/flowschannels/ColdFlow.kt
@@ -0,0 +1,53 @@
+package com.example.flowschannels
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+/**
+ * flow builder generate a cold flow.
+ * 1. It emits data only when there is a collector.
+ * 2. It doesn't store the data
+ * 3. It can't have multiple collector.
+ */
+
+private suspend fun producer() =  flow<Int> {
+    repeat(5) {
+        delay(1000)
+        println("Emitted: $it")
+        emit(it)
+    }
+}
+
+fun coldFlow() = runBlocking {
+    withContext(Dispatchers.IO) {
+        val data : Flow<Int> = producer()
+        println("Consumer Coroutine: Launched")
+        val  job = CoroutineScope(Dispatchers.IO).launch {
+            //No collector - no emit
+            data.collect { value ->
+                println("Collected 1st: $value")
+            }
+            //A new fresh flow of data will start for each collector.
+            data.collect { value ->
+                println("Collected 2nd: $value")
+            }
+        }
+        delay(2500)
+        //When job is cancelled, collector will unsubscribe the flow.
+        // Then emition of data will stop from cold flow.
+        job.cancel()
+    }
+}
+
+/*
+fun main() {
+    println("Calling: coldFlow()")
+    coldFlow()
+}
+*/
---
+++ FlowsChannels/app/src/main/java/com/example/flowschannels/HotFlow.kt
@@ -0,0 +1,64 @@
+package com.example.flowschannels
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+
+/**
+ * MutableStateFlow generate a hot flow.
+ * 1.Needs an initial value and emits it as soon as the collector starts collecting.
+ * 2. It emits data even when there is no collector.
+ * 3. It can store the data
+ * 4. It can have multiple collector. (1 to N)
+ * 5. It has value property, which keeps history of last known value.
+ * 6. It does not emit consecutive repeated values.
+ * 7. It emits last know value.
+ * 8. Similar to LiveData except for the Lifecycle awareness of the Android component.
+ * We should use repeatOnLifecycle scope with StateFlow to add the Lifecycle awareness to it,
+ * then it will become exactly like LiveData.
+ */
+private suspend fun producer(): MutableStateFlow<Int> {
+    val mutableStateFlow = MutableStateFlow<Int>(-1)
+    CoroutineScope(Dispatchers.IO).launch {
+        repeat(5) {
+            println("Emitted: $it")
+            mutableStateFlow.value = it
+            delay(1000)
+        }
+    }
+
+    return mutableStateFlow
+}
+
+
+fun hotFlow() = runBlocking {
+    withContext(Dispatchers.IO) {
+        val data: MutableStateFlow<Int> = producer()
+        println("Consumer Coroutine: Launched")
+        CoroutineScope(this.coroutineContext).launch {
+            data.collect { value ->
+                println("Collected 1st: $value")
+            }
+        }
+        delay(2500)
+        CoroutineScope(this.coroutineContext).launch {
+            data.collect { value ->
+                println("Collected 2nd: $value")
+            }
+        }
+    }
+}
+
+fun main() {
+    println("Calling: hotFlow()")
+    hotFlow()
+}
+
+
